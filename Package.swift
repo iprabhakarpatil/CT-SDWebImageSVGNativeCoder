8abc00426949ec5dd98897c63baae6a083d84cd2
Prabhakar Patil <12987010+iprabhakarpatil@users.noreply.github.com>
2025-09-17 18:38:31 +0530

    Update Package.swift
    updating to support CT integrataion

--- Package.swift
+++ Package.swift
@@ -17,15 +17,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.10.0"),
-        .package(url: "https://github.com/SDWebImage/svgnative-Xcode.git", from: "0.1.0")
+       // .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.10.0"),
+       .package(url: "https://github.com/SDWebImage/svgnative-Xcode.git", from: "0.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SDWebImageSVGNativeCoder",
-            dependencies: ["SDWebImage", "svgnative"],
+            dependencies: ["svgnative"],
             path: ".",
             sources: ["SDWebImageSVGNativeCoder/Classes"],
             publicHeadersPath: "SDWebImageSVGNativeCoder/Classes",
